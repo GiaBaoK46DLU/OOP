b64d165e858d3b88eeaac9e2790322904c141eb5
GiaBaoK46DLU <dlgb260704@gmail.com>
2024-04-19 17:05:24 +0700

    Update

---
+++ 2212343_DLGBao_Lab3_QuanLyPhanSo/Main.cs
@@ -0,0 +1,27 @@
+﻿using _2212343_DLGBao_Lab3_QuanLyPhanSo;
+
+PhanSo a = new PhanSo(2, 3);
+a.Xuat();
+
+PhanSo b = new PhanSo(3, 4);
+b.Xuat();
+
+PhanSo c = a.Cong(a, b);
+Console.Write(" a+b ");
+c.Xuat();
+
+PhanSo d = a.Tru(a, b);
+Console.Write(" a-b ");
+d.Xuat();
+
+PhanSo e = a.Nhan(a, b);
+Console.Write(" a*b ");
+e.Xuat();
+
+PhanSo f = a.Chia(a, b);
+Console.Write(" a/b ");
+f.Xuat();
+
+MangPhanSo m = new MangPhanSo();
+m.Nhap();
+m.Xuat();
